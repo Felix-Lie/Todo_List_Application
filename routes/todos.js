bc2aaf9f767fe1a37a7ae1b782b3de01fb48ddf5
Felix lie <felixlie19@gmail.com>
2022-05-04 23:27:30 -0700

    Defining Delete Route

--- routes/todos.js
+++ routes/todos.js
@@ -46,4 +46,15 @@ router.put('/:todoId', function (req, res) {
     });
 });
 
+//Delete Route
+router.delete('/:todoId', function (req, res) {
+  db.Todo.remove({ _id: req.params.todoId })
+    .then(function () {
+      res.json({ message: 'We deleted it!' });
+    })
+    .catch(function (err) {
+      res.send(err);
+    });
+});
+
 module.exports = router;
