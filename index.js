ac50d90b4d6d526977c47aa10c3ce6f657ce1d85
Felix lie <felixlie19@gmail.com>
2022-05-04 22:47:41 -0700

    Defining Create Route

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express'),
-  app = express();
+  app = express(),
+  bodyParser = require('body-parser');
 
 var todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//these two lines allow us to access request body for post req
 
 app.get('/', function (req, res) {
   res.send('HELLO FROM THE ROOT ROUTE');
--- routes/todos.js
+++ routes/todos.js
@@ -2,10 +2,22 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models');
 
+//index route
 router.get('/', function (req, res) {
   db.Todo.find()
     .then(function (todos) {
       res.json(todos);
+    })
+    .catch(function (err) {
+      res.send(err);
+    });
+});
+
+//create route
+router.post('/', function (req, res) {
+  db.Todo.create(req.body)
+    .then(function (newTodo) {
+      res.status(201).json(newTodo);
     })
     .catch(function (err) {
       res.send(err);
