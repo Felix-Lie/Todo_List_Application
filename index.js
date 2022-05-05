8bebceb3d2343848805667754696ebf5a30f0806
Felix lie <felixlie19@gmail.com>
2022-05-05 00:22:07 -0700

    Serving Static Files

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ var todoRoutes = require('./routes/todos');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //these two lines allow us to access request body for post req
+app.use(express.static(__dirname + '/public'));
+//allows to connect with css
+app.use(express.static(__dirname + '/views'));
+//allows to connect with index.html
 
 app.get('/', function (req, res) {
-  res.send('HELLO FROM THE ROOT ROUTE');
+  res.sendFile('index.html');
 });
 
 app.use('/api/todos', todoRoutes);
---
+++ public/app.js
@@ -0,0 +1,1 @@
+alert('HI FROM APP>JS');
