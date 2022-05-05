4c47493f502d0f5846af60b1e595ed0dc2a3247a
Felix lie <felixlie19@gmail.com>
2022-05-05 09:58:17 -0700

    Connecting Form to API

--- public/app.js
+++ public/app.js
@@ -1,15 +1,39 @@
 // global $
 $('document').ready(function () {
   $.getJSON('/api/todos').then(addTodos);
+
+  $('#todoInput').keypress(function (event) {
+    if (event.which == 13) {
+      createTodo();
+    }
+  });
 });
 
 function addTodos(todos) {
   //add todos to page here
   todos.forEach(function (todo) {
-    var newTodo = $('<li class="task">' + todo.name + todo.completed + '</li>');
-    if (todo.completed) {
-      newTodo.addClass('done');
-    }
-    $('.list').append(newTodo);
+    addTodo(todo);
   });
+}
+
+function addTodo(todo) {
+  var newTodo = $('<li class="task">' + todo.name + '</li>');
+  if (todo.completed) {
+    newTodo.addClass('done');
+  }
+  $('.list').append(newTodo);
+}
+
+function createTodo() {
+  //send request to create new todo
+  var usrInput = $('#todoInput').val();
+  console.log(usrInput);
+  $.post('/api/todos', { name: usrInput })
+    .then(function (newTodo) {
+      $('#todoInput').val('');
+      addTodo(newTodo);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
